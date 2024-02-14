9a3adf896ab965886964b90de22c222342c1d696
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2024-02-14 09:49:29 -0600

    code review #3

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ const profileDescriptionInput = document.querySelector(
 
 // const modalOpenSelector = "modal_opened"; a shortcut for later
 // const profileForm = document.forms["profile-form"]; improvement as needed by reviewer for later
-const profileEditForm = profileEditModal.querySelector(".modal__form"); //removing this line of code removes all the cards from the webpage//
+const profileEditForm = profileEditModal.querySelector(".modal__form"); //as directed in Proj4 Stage2 Video by Julian; removing this line of code removes all the cards from the webpage//
 const cardListEl = document.querySelector(".cards__list");
 console.log("cardListEl", cardListEl);
 const cardTemplate =
@@ -50,15 +50,22 @@ const cardTemplate =
 function closePopup() {
   profileEditModal.classList.remove(
     "modal_opened"
-  ); /* used a modificator with only one low dash */
+  ); /* used a modificator with only one low dash as reviewer directed */
 }
 function getCardElement(cardData) {
+  //clone the template element with all its content and store it in a cardElement
   const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the object
+  //set the image field alt text tothe name field of the object
+  //set the card title to the name field of the object, too
   cardTitleEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
+  // return cardElement;
   return cardElement;
 }
 
@@ -86,7 +93,25 @@ profileEditCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
+// as shown in Julian Hernandez's Project4 Stage4 3/3 Video
+// for (let i = 0; i < initialCards.length; i++) {
+//   console.log("hello" + i);
+//   const card = initialCards(i);
+// }
+
+// initialCards.forEach((cardData) => {
+//   const cardElement = getCardElement(cardData);
+//   cardListEl.append(cardElement);
+// });
+
 initialCards.forEach((cardData) => {
+  console.log(cardData.name);
+
   const cardElement = getCardElement(cardData);
-  cardListEl.append(cardElement);
+  cardListEl.prepend(cardElement);
+  //append makes the cards go in reverse order
+  //prepend makes the cards go chronological order
 });
+
+//extra notes below:
+// cardTitleEl.textContext = cardData.title;
