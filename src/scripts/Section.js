8c982d02cba6acd40025fc81f3f0e8db166f54f7
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2024-08-11 12:03:48 -0500

    Section.js done

---
+++ src/scripts/Section.js
@@ -0,0 +1,19 @@
+import { container } from "webpack";
+
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._container = document.querySelector(containerSelector);
+    this._renderer = renderer;
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
