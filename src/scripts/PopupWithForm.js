5ca88162da8e506fb36762516a81bf509fe7cece
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2024-08-11 11:22:33 -0500

    PopupWithForm done

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,10 +1,26 @@
 import Popup from "./Popup.js";
 
-class PopupWithForm extends Popup {
+export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super({ popupSelector });
     this._popupForm = this._popupElement.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
+    this._inputValues = this._form.querySelectorAll(".modal__imput");
+  }
+
+  _getInputValues() {
+    const inputData = {};
+    inputValues.forEach((input) => {});
+    return inputData;
+  }
+
+  setEventListeners() {
+    this._form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+      this._form.reset();
+    });
+    super.setEventListeners();
   }
 
   close() {
