cca44317bb3d2414a77a68a5ed9a2c7de0245643
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2024-08-25 15:45:54 -0500

    Aug25 Octavio helped with popup.js tryign to close image popups with clicking outside of the card

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -14,11 +14,22 @@ export default class Popup {
   open() {
     this._popupElement.classList.add("modal_opened");
     document.addEventListener("keydown", this._handleEscClose);
+    this._popupElement.addEventListener("mousedown", this.__handleClickOutside);
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
     document.removeEventListener("keydown", this._handleEscClose);
+    this._popupElement.removeEventListener(
+      "mousedown",
+      this.__handleClickOutside
+    );
+  }
+
+  __handleClickOutside(event) {
+    if (event.target.classList.contains("modal_opened")) {
+      this.close();
+    }
   }
 
   _handleEscClose = (evt) => {
